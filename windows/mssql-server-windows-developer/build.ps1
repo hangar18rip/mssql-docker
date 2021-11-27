82c6005f7d187a38fe617e69ef7878ce9fb5bf5e
Olivier Delmotte <olivier@odelmotte.fr>
2021-11-27 08:03:45 -0800

    fix docker build

--- windows/mssql-server-windows-developer/build.ps1
+++ windows/mssql-server-windows-developer/build.ps1
@@ -1,13 +1,18 @@
+[CmdletBinding()]
+param (
+)
+
 Set-Location $psscriptroot
 
-$setup = Join-Path $env:TEMP "sqlsetup.exe"
-
-Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=866662" -OutFile $setup -UseBasicParsing
-
+$ErrorActionPreference = "STOP"
 # Get-Item $setup
 
-$process = Start-Process -Wait -FilePath $setup -ArgumentList @("/Quiet", "/Action=Download", "/Language=en-US", "/MediaType=CAB", "/MediaPath=$psscriptroot") -PassThru
+# Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=866662" -OutFile $setup -UseBasicParsing
+# $process = Start-Process -Wait -FilePath $setup -ArgumentList @("/Quiet", "/Action=Download", "/Language=en-US", "/MediaType=CAB", "/MediaPath=$psscriptroot") -PassThru
 
-Write-Host $process.ExitCode
+#   Write-Host $process.ExitCode
 
-docker build --memory 4g -t sqldev .
+docker build --memory 4g -t odesacr.azurecr.io/sqldev .
+
+az acr login --name odesacr
+docker push odesacr.azurecr.io/sqldev
